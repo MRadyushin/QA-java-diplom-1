61eb9fbe919cfca49aa86f5ef79a316acbd7ab4d
Maksim Radyushin <m.radyushin@yandex.ru>
2022-11-26 00:21:59 +0300

    add IngredientTests
    + @before
    + Test getNameReturnCorrectValue

---
+++ src/test/java/IngredientTests.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import praktikum.*;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientTests {
+
+    private final String ingredientTypeValue;
+    private final String name;
+    private final float price;
+
+    private Ingredient ingredient;
+    private IngredientType ingredientType;
+
+    public IngredientTests(String ingredientTypeValue, String name, float price) {
+        this.ingredientTypeValue = ingredientTypeValue;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name = "Тип ингридиента: {0}/ Название ингридиента: {1}/ Цена ингридиента: {2}")
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"FILLING", "sausage", 300f},
+                {"SAUCE", "sour cream", 200f},
+        };
+    }
+    @Before
+    public void setUp() {
+        this.ingredient = new Ingredient(this.ingredientType.valueOf(ingredientTypeValue), name, price);
+    }
+
+    @Test
+    public void getNameReturnCorrectValue() {
+        String actualName = ingredient.getName();
+        assertEquals("Name Ingredient is incorrect", actualName, name);
+    }
+
+}
